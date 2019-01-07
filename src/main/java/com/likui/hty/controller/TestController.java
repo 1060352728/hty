a8fc499a52446ab0eb31fc770d5ed904b586df18
likui <1060352728@qq.com>
2019-01-07 22:10:54 +0800

    加入swagger接口文档

--- src/main/java/com/likui/hty/controller/TestController.java
+++ src/main/java/com/likui/hty/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.likui.hty.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,30 +16,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @ApiOperation(value = "测试admin的权限")
     @RequestMapping("/admin")
     @PreAuthorize("hasAnyRole('ADMIN')")
     public String admin(){
         return "admin";
     }
 
+    @ApiOperation(value = "测试find的权限")
     @RequestMapping("/find")
     @PreAuthorize("hasAnyRole('USER_SELECT')")
     public String find(){
         return "find";
     }
 
+    @ApiOperation(value = "测试set的权限")
     @RequestMapping("/set")
     @PreAuthorize("hasAnyRole('USER_CREATE')")
     public String set(){
         return "set";
     }
 
+    @ApiOperation(value = "测试update的权限")
     @RequestMapping("/update")
     @PreAuthorize("hasAnyRole('USER_EDIT')")
     public String update(){
         return "update";
     }
 
+    @ApiOperation(value = "测试delete的权限")
     @RequestMapping("/delete")
     @PreAuthorize("hasAnyRole('USER_DELETE')")
     public String delete(){
